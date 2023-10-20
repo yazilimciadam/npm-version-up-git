db9086873d2d3775f9a9bd09ab707fecf583d5ce
Mehmet TAŞ <tasmehmet25@outlook.com>
2023-10-20 05:05:45 +0300

    format abiiii

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  core.setOutput("name", nameToGreet)
+  core.setOutput("name", nameToGreet);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
